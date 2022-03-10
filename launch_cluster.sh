7232bd0b5682a02cd0820fdef29eb60ce136afc3
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-03-10 12:40:35 +0100

    Update launch_cluster.sh

--- launch_cluster.sh
+++ launch_cluster.sh
@@ -3,4 +3,4 @@
 #$ -cwd
 source activate py365
 
-python src/main.py --input .graph/kg_semart.csv --output emb/semart.emd --dimensions 1028
+python src/main.py --input .graph/kg_semart.csv --output emb/semart.emd --dimensions 1028 >salida.txt
